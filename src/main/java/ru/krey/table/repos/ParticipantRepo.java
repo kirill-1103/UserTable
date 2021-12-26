8dd80e0acffe50714ae63b7acd10427e2674c986
kirill-1103 <kreysmann.kirya@mail.ru>
2021-12-26 04:18:25 +0300

    change db

--- src/main/java/ru/krey/table/repos/ParticipantRepo.java
+++ src/main/java/ru/krey/table/repos/ParticipantRepo.java
@@ -1,13 +1,10 @@
 package ru.krey.table.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+
 import ru.krey.table.model.Participant;
 
 public interface ParticipantRepo extends JpaRepository<Participant,Long> {
     Participant findByLogin(String login);
 
-//    @Modifying
-//    void deleteByLogin(String login);
 }
